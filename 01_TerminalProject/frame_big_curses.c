344319457d4dafadd5ca19f050c31b9981aea6c7
StephenMalchevskii <usamstudent21@gmail.com>
2024-09-21 15:07:05 +0300

    fix 01 lection

--- 01_TerminalProject/frame_big_curses.c
+++ 01_TerminalProject/frame_big_curses.c
@@ -3,6 +3,7 @@
 
 int main(void) {
 	WINDOW* win;
+	WINDOW* frame;
 	char c = 0;
 
 	setlocale(LC_ALL, "");
@@ -15,16 +16,21 @@ int main(void) {
 	printw("window:"); // TODO: переделать пример - мы рисуем бокс внутри окна и перекрывает первую строку
 	refresh();
 
+	frame = newwin(LINES - 8, COLS - 18, 4, 9);
+    box(frame, 0, 0);
+    mvwaddstr(frame, 0, (int)((COLS - 25) / 2), "Рамка");
+    wrefresh(frame);
+
 	win = newwin(LINES - 10, COLS - 20, 5, 10);
 	keypad(win, TRUE);
 	scrollok(win, TRUE);
 
-	box(win, 0, 0); // TODO: Переделать пример: у нас скроллится box
-	wmove(win, 1, 1);
+	// box(win, 0, 0); // TODO: Переделать пример: у нас скроллится box
+	// wmove(win, 1, 1);
 
 	while((c = wgetch(win)) != 27) {
 		wprintw(win, "  %d: %s\n", c, keyname(c));
-		box(win, 0, 0);
+		// box(win, 0, 0);
 		wrefresh(win);
 	}
 
